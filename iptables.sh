756e04ab530c89bb8a1ad834525718132459ce6f
kali <adamalwan55@gmail.com>
2020-10-16 20:25:02 -0400

    first commit

---
+++ iptables.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo -e "1)ifconfig wlan1"
+echo -e "2)iptables "
+echo -e "3)ip_forward "
+echo -e "4)clear "
+
+
+while true; do
+read -p "please Enter Yor Option > " adam
+if [[ $adam -eq '1' ]]; then
+echo -e "Whait...!"
+sleep 2
+sudo ifconfig wlan1 up 192.168.1.1 netmask 255.255.255.0
+sudo route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1
+read -p "please Enter Yor Option > " adam
+elif [[ $adam -eq '2' ]]; then
+echo -e "Whait...!"
+sleep 2
+sudo iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
+sudo iptables --append FORWARD --in-interface wlan1 -j ACCEPT
+read -p "please Enter Yor Option > " adam
+elif [[ $adam -eq '3' ]]; then
+echo -e "Whait...!"
+sleep 2
+sudo echo 1 > /proc/sys/net/ipv4/ip_forward
+read -p "please Enter Yor Option > " adam
+elif [[ $adam -eq '4' ]]; then
+echo -e "Whait...!"
+sleep 2
+clear
+echo -e "Bye...!"
+else
+echo "Wrong Options..!"
+fi
+done
---
+++ scr.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo -e "1)Update System "
+echo -e "2)Check Program nedd Upgrade "
+echo -e "3)Upgrade  Program "
+echo -e "4)Autoremove "
+echo -e "5)Autoclean "
+echo -e "0)Exit "
+
+while true; do
+read -p "please Enter Yor Option > " tryag
+if [[ $tryag -eq '1' ]]; then
+echo -e "Whait...!"
+sleep 2
+sudo apt-get update
+read -p "please Enter Yor Option > " tryag
+elif [[ $tryag -eq '2' ]]; then
+echo -e "Whait...!"
+sleep 2
+apt-get list --upgradable
+read -p "please Enter Yor Option > " tryag
+elif [[ $tryag -eq '3' ]]; then
+echo -e "Whait...!"
+sleep 2
+apt-get upgrade
+read -p "please Enter Yor Option > " tryag
+elif [[ $tryag -eq '4' ]]; then
+echo -e "Whait...!"
+sleep 2
+apt autoremove
+read -p "please Enter Yor Option > " tryag
+elif [[ $tryag -eq '5' ]]; then
+echo -e "Whait...!"
+sleep 2
+apt autoclean
+read -p "please Enter Yor Option > " tryag
+elif [[ $tryag -eq '6' ]]; then
+echo -e "Whait...!"
+sleep 2
+exit 0
+echo -e "Bye...!"
+else
+echo "Wrong Options..!"
+fi
+done
